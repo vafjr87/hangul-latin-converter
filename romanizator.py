789a63ed50b71627cc2dc04284616368b29be1b5
vafjr87 <vafjr87@gmail.com>
2017-05-09 21:30:37 -0300

    Not Version 1.0 yet:
    - Buffering in terminal, now it accepts full text, like lyrics, try some k-pop songs ;) (press CTRL + D after paste)
    - Creation of a class for better maintenance
    - Solved capital letters issue
    - Serialization of the dictionary data
    
    - Still needs:
    - Splitting data in 자음, 모음 and 받침
    - 받침 exception rules
    - Maybe something else I still didn't figured out

--- romanizator.py
+++ romanizator.py
@@ -1,119 +1,70 @@
 import korean.hangul as kr
 import sys
+import pickle
 
-hangul_roman = {
-				'ㄱ' : 'g',
-				'ㄴ' : 'n',
-				'ㄷ' : 'd',
-				'ㄹ' : 'r',
-				'ㅁ' : 'm',
-				'ㅂ' : 'b',
-				'ㅅ' : 's',
-				'ㅇ' : '',
-				'ㅈ' : 'j',
-				'ㅊ' : 'ch',
-				'ㅋ' : 'k',
-				'ㅌ' : 't',
-				'ㅍ' : 'p',
-				'ㅎ' : 'h',
-				'ㄲ' : 'kk',
-				'ㄸ' : 'tt',
-				'ㅃ' : 'pp',
-				'ㅆ' : 'ss',
-				'ㅉ' : 'jj',
-				'ㅏ' : 'a',
-				'ㅑ' : 'ya',
-				'ㅓ' : 'eo',
-				'ㅕ' : 'yeo',
-				'ㅗ' : 'o' ,
-				'ㅛ' : 'yo',
-				'ㅜ' : 'u',
-				'ㅠ' : 'yu', 
-				'ㅡ' : 'eu' ,
-				'ㅣ' : 'i',
-				'ㅐ' : 'ae',
-				'ㅒ' : 'yae',
-				'ㅔ' : 'e',
-				'ㅖ' : 'ye',
-				'ㅘ' : 'wa',
-				'ㅙ' : 'wae',
-				'ㅚ' : 'oe' ,
-				'ㅝ' : 'wo',
-				'ㅟ' : 'wi',
-				'ㅞ' : 'we',
-				'ㅢ' : 'ui'
-				}
+class romanizator(object):
 
-batchim = {
-			'ㄱ' : 'k',
-			'ㄴ' : 'n',
-			'ㄷ' : 't',
-			'ㄹ' : 'l',
-			'ㅁ' : 'm',
-			'ㅂ' : 'p',
-			'ㅅ' : 't',
-			'ㅇ' : 'ng',
-			'ㅈ' : 't',
-			'ㅊ' : 't',
-			'ㅋ' : 'k',
-			'ㅌ' : 't',
-			'ㅍ' : 'p',
-			'ㅎ' : 't',
-			'ㄲ' : 'k',
-			'ㅆ' : 't',
-			'ㄺ': 'lg',
-			'ㅄ': 'ps',
-			'ㄵ': 'nj',
-			'ㄼ': 'lb',
-			'ㅀ': 'l',
-			'ㄶ': 'n',
-			'ㄾ': 'lt,',
-			'ㄻ': 'lm,',
-			'ㄿ': 'lp',
-			'ㄳ': 'g'
-}
+	def __init__(self):
+		print('Welcome to the Hangul->Latin Script Conversor 0.9\n')
+		self.input = self.get_input()
+		self.hangul = pickle.load(open('./hangul.dat', "rb"))
+		self.output = ''
 
-def romanize(sentence):
-	blocks = list(sentence)
-	romanized_sentence = ''
-	upper = True
+	def get_input(self):
+		print('Please insert the korean text and press CTRL + D:\n')
 
-	for block in blocks:
-		if kr.is_hangul(block):
-			letters = kr.split_char(block)
-			
-			for i in range(len(letters)):
-				if i == 2:
-					if letters[i] != '' and letters[i] in batchim:
-						romanized_sentence = romanized_sentence + batchim[letters[i]]
+		return sys.stdin.read().split('\n')
+
+	def romanize(self):
+
+		romanized_output = list()
+
+		for sentence in self.input:
+			romanized_sentence = ''
+			upper = True
+
+			blocks = list(sentence)
+
+			for block in blocks:
+				if kr.is_hangul(block):
+					letters = kr.split_char(block)
+					
+					for i in range(len(letters)):
+						if i == (len(letters) - 1): # If it's batchim
+							if letters[i] != '' and letters[i] in self.hangul['batchim']:
+ 								romanized_sentence = romanized_sentence + self.hangul['batchim'][letters[i]]
+						else:
+							if upper:
+								if len(self.hangul['letters'][letters[i]]) > 1:
+									romanized_sentence += str.upper(self.hangul['letters'][letters[i]][0]) + \
+																	self.hangul['letters'][letters[i]][1:]
+								else:
+									romanized_sentence += str.upper(self.hangul['letters'][letters[i]])
+								
+								if romanized_sentence != '':
+									upper = False
+							else:
+								romanized_sentence += self.hangul['letters'][letters[i]]
+
 				else:
-					if upper:
-						romanized_sentence = romanized_sentence + str.upper(hangul_roman[letters[i]])
-						upper = False
+					if block in '.!?':
+						upper = True
 					else:
-						romanized_sentence = romanized_sentence + hangul_roman[letters[i]]
-		else:
-			if block in '.!?':
-				upper = True
+						upper = False
 
-			romanized_sentence = romanized_sentence + block
+					romanized_sentence += block
 
-	# return str.upper(romanized_sentence[0] + romanized_sentence[1:]
-	return(romanized_sentence.strip())
+			romanized_output.append(romanized_sentence.strip())
 
-print('Welcome to the Hangul->Latin Script Conversor 0.9\n')
-print('Please insert the korean text:\n')
+		self.output = romanized_output
 
-lines = []
+	def print_output(self):
+		print('\nRomanized version:\n')
 
-while True:
-	line = input()
-	if line:
-		lines.append(line)
-	else:
-		break
+		for line in self.output:
+			print(line)
 
-for sentence in lines:       
-	print(romanize(sentence))
 
-print()
+r = romanizator()
+r.romanize()
+r.print_output()
